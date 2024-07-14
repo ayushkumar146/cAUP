453192eaee6352ea64e046552cb11f11076f5fff
ayushkumar146 <108751760+ayushkumar146@users.noreply.github.com>
2024-07-14 19:11:16 +0530

    Delete pypro/AUP/onju directory

--- pypro/AUP/onju/dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from dashboard.views import user_homepage
-
-urlpatterns = [
-    path("Dashboard/<int:user_id>", user_homepage, name="user-homepage"),
-    # path("polls/<int:poll_id>/", poll_detail, name="poll-detail"),
-
-]
